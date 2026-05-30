d954fb03bb0e4dbce7995cf3314151926057f605
Nick <leftythedrummer88@gmail.com>
2026-05-30 14:58:19 -0600

    Added more creatures to database

---
+++ creature_database.py
@@ -0,0 +1,30 @@
+CREATURES = {
+    "goblin": {
+        "name": "Goblin",
+        "description": "A small, green humanoid with a mischievous grin. They are quick and cunning, often attacking in groups.",
+        "hp": 15,
+        "attack": 4,
+        "xp_reward": 35
+    },
+    "skeleton": {
+        "name": "Skeleton",
+        "description": "A reanimated corpse held together by dark magic. They are relentless and can be dangerous in large numbers.",
+        "hp": 10,   
+        "attack": 5,
+        "xp_reward": 40
+    },
+    "orc": {
+        "name": "Orc",
+        "description": "A brutish, muscular creature with a savage temperament. They are stronger than goblins and often serve as enforcers for more powerful beings.",
+        "hp": 30,
+        "attack": 7,
+        "xp_reward": 75
+    },
+    "troll": {
+        "name": "Troll",
+        "description": "A large, brutish creature with thick skin and immense strength. They are slow but can deal devastating blows.",
+        "hp": 50,
+        "attack": 10,
+        "xp_reward": 100
+    }
+}
